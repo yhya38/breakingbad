57f6c8a6032a732171ce654d277f29db44178e31
yk2217 <yahiyakhan2217@gmail.com>
2020-08-20 13:05:00 +0530

    modified cards

--- src/App.js
+++ src/App.js
@@ -1,37 +1,37 @@
-import React from 'react';
-import './App.css';
-import BreadingCard from './BreadingCard';
-import  API_URL  from './Api';
-
+import React from "react";
+import "./App.css";
+import BreadingCard from "./BreadingCard";
+import API_URL from "./Api";
 
 class App extends React.Component {
-
   state = {
-    feeds: []
+    feeds: [],
   };
 
   fetchbreakingbad = () => {
     fetch(API_URL, {
-      method: "GET"
+      method: "GET",
     })
-      .then(data => data.json())
-      .then(response => this.setState({ feeds: response }))
-    .catch(error=>console.log(error))
-  }
+      .then((data) => data.json())
+      .then((response) => this.setState({ feeds: response }))
+      .catch((error) => console.log(error));
+  };
   componentDidMount() {
-  this.fetchbreakingbad()
-}
-  render(){
-  return (
-    <div className="App">
-      {
-        this.state.feeds.map(obj => (
-          <BreadingCard data={obj}/>
-        ))
-      }
-      
-    </div>
-  );
+    this.fetchbreakingbad();
+  }
+  render() {
+    return (
+      <div>
+        <h1 className="breaking__top">Breaking Bad API</h1>
+        <div className="App">
+          {this.state.feeds.length > 0 ? (
+            this.state.feeds.map((obj) => <BreadingCard data={obj} />)
+          ) : (
+            <h1 className="breaking">Loading...</h1>
+          )}
+        </div>
+      </div>
+    );
   }
 }
 
